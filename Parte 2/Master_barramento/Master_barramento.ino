e978500031958ae1cd93580a9c14d6b94c80ecbb
gabrielrb1 <32144610+gabrielrb1@users.noreply.github.com>
2018-05-17 00:29:58 -0300

    Add files via upload
    Primeiro teste de comunicação serial entre mestre e escravo. Foi utilizado o Arduino como mestre, de forma provisória. Criados doi comandos para serem lidos pelo nodemcu

---
+++ Parte 2/Master_barramento/Master_barramento.ino
@@ -0,0 +1,23 @@
+void setup() {
+Serial.begin(9600);
+pinMode(7,OUTPUT); //string que receberá a ação a ser feita pelo usuário
+}
+
+void loop() {
+String acao = Serial.readString();  
+if(Serial.available()){
+//Biblioteca de comandos
+//SA -> Slave Act, onde o escravo acende um LED
+//SR -> Slave Read, leitura de uma porta do escravo
+  if (acao == "SA"){
+    Serial.print("A");
+    delay(10);  
+  }
+  if (acao == "SR"){
+    Serial.print("R");
+    delay(10);
+  }
+}
+
+
+}
---
+++ Parte 2/Slave_barramento/Slave_barramento.ino
@@ -0,0 +1,26 @@
+//#include <ESP8266WiFi.h>
+//Foram inseridos dois LED's, para testar os comandos SA e SR
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(13,OUTPUT); //LED do SA
+  pinMode(15,OUTPUT); //LED do SR
+}
+
+void loop() {
+  String ordem;
+  if (Serial.available()){
+    ordem = Serial.read();
+    if (ordem == "A"){
+      digitalWrite(13,HIGH);
+      delay(2000);
+      digitalWrite(13,LOW);
+    }
+    if (ordem == "R"){
+      digitalWrite(15,HIGH);
+      delay(2000);
+      digitalWrite(15,LOW);
+    }
+  }
+
+}
