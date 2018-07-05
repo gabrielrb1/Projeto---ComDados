4366b9cf003a16393e9dcb479c898b676d068d62
gabrielrb1 <32144610+gabrielrb1@users.noreply.github.com>
2018-07-05 18:34:06 -0400

    Add files via upload

---
+++ Master_barramento/Master_barramento.ino
@@ -0,0 +1,56 @@
+const int trans = 16;
+
+void setup() {
+Serial.begin(9600);
+Serial.setTimeout(100);
+pinMode(trans,OUTPUT);
+digitalWrite(trans,HIGH);//Habilita a escrita do mestre no canal
+}
+
+void loop() {
+if(Serial.available()){  
+String acao = Serial.readString();  
+//Biblioteca de comandos
+//n = número do escravo
+//SnW -> Slave Write, onde o escravo acende um LED
+//SnR -> Slave Read, leitura de um sensor do escravo
+  if (acao == "S1W"){
+    Serial.print("1");
+    delay(10);  
+    Serial.println("W");
+    delay(10);
+  }
+  if (acao == "S2W"){
+    Serial.print("2");
+    delay(10);  
+    Serial.println("W");
+    delay(10);
+  }
+  if (acao == "S1R"){
+    digitalWrite(trans,LOW);
+    Serial.print("1");
+    delay(10);  
+    Serial.println("R");
+    Serial.flush();
+    if (Serial.find("ACK")){
+      delay(15);
+      int temperatura = Serial.parseInt();
+      Serial.println(temperatura);
+    }
+    digitalWrite(trans,HIGH);
+  }
+  if (acao == "S2R"){
+    digitalWrite(trans,LOW);
+    Serial.print("2");
+    delay(10);  
+    Serial.println("R");
+    Serial.flush();
+    if (Serial.find("ACK")){
+      delay(15);
+      int temperatura = Serial.parseInt();
+      Serial.println(temperatura);
+    }
+    digitalWrite(trans,LOW);
+  }
+}
+}
---
+++ Slave_barramento/Slave_barramento.ino
@@ -0,0 +1,41 @@
+const int led = 4;
+const int trans = 8;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);//pino de acionamento do LED
+  pinMode(A0,INPUT);//leitura do sensor
+  pinMode(trans,INPUT);//pino ligado aos terminais DE e RE do transceiver
+  digitalWrite(led,LOW);
+  digitalWrite(trans,LOW);//escravo começa 
+  //DE e RE = 0 -> transceiver recebe dados
+  //DE e RE = 1 -> transceiver envia dados
+}
+
+void loop() {
+  if (Serial.available()){
+    if (Serial.read() == '2'){//Definição do ID do escravo
+      delay(15);
+      char ordem = Serial.read();
+      if (ordem == 'W'){
+        if (digitalRead(led)==LOW){
+        digitalWrite(led,HIGH);
+        //Serial.print("LED ligado");
+        }
+        else{
+        digitalWrite(led,LOW);
+        //Serial.print("LED foi desilgado");
+        }
+    }
+    if (ordem == 'R'){
+      int temp = analogRead(A0);
+      digitalWrite(trans,HIGH);
+      Serial.print("ACK"); //confirmação de recebimento do comando de leitura
+      delay(10);
+      Serial.print(temp);
+      Serial.flush();
+      digitalWrite(trans,LOW);
+    }
+  }
+  }
+}
