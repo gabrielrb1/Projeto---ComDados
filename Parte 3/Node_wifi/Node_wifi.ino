92b785a7d615fa6f49a3614791763f4296f88329
gabrielrb1 <32144610+gabrielrb1@users.noreply.github.com>
2018-07-08 23:43:24 -0400

    Add files via upload

--- Parte 3/Node_wifi/Node_wifi.ino
+++ Parte 3/Node_wifi/Node_wifi.ino
@@ -1,23 +1,14 @@
 #include <ESP8266WiFi.h>
-#include <SoftwareSerial.h>
 
 const char* ssid = "COMDADOS_RS485";
 const char* password = "12345678";
 WiFiServer server(80);
 
-#define SSerialRX        13  //Serial Receive pin
-#define SSerialTX        15  //Serial Transmit pin
-#define pinLED         7
-
-SoftwareSerial RS485Serial(SSerialRX, SSerialTX); // RX, TX
-
 int byteReceived;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600); 
-  pinMode(SSerialTxControl,OUTPUT);
-  pinMode(pinLED,OUTPUT);
+  Serial.begin(9600);
   
   IPAddress staticIP(192, 168, 4, 2); // IP set to Static
   IPAddress gateway(192, 168, 4, 1);// gateway set to Static
@@ -34,7 +25,6 @@ void setup() {
   Serial.println(WiFi.softAPIP());
 
   Serial.begin(9600);
-  RS485Serial.begin(4800);
 }
 
 void loop() {
@@ -51,34 +41,15 @@ void loop() {
   String req = client.readStringUntil('\r');
   req = req.substring(req.indexOf("/") + 1, req.indexOf("HTTP") - 1);
   Serial.println(req);
+  Serial.flush();
   client.flush();
-
   // Match the request
-
-  if (req.indexOf("Liga") != -1)
-  {
-    client.print("Ligando LED na porta 13  ");
-    digitalWrite(13,HIGH);
-    delay(1000);
-    digitalWrite(13,LOW);
-    delay(1000);
-    digitalWrite(13,HIGH);
-    delay(1000);
-    digitalWrite(13,LOW);
-    delay(1000);
-    digitalWrite(13,HIGH);
-    delay(1000);
-    digitalWrite(13,LOW);
-    delay(1000);
+  if (req.indexOf("1") != -1){
+    client.print("Ligando atuador");
   }
-  else {
-    client.print("Invalid Request");
-    client.flush();
-    client.stop();
-    return;
+  if (req.indexOf("2") != -1){
+    client.print("Lendo sensor");
   }
-
-  client.print("HTTP/1.1 200 OK\n\n");
   client.flush();
 
 }
